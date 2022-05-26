d917a233dee75274b37f5b8d1e49b5ce684af14d
MLH <haoooliu@gmail.com>
2022-05-26 09:07:12 +0800

    20220526

---
+++ .idea/servercheck/sqlscripts/sql_fastrecovery.sql
@@ -0,0 +1,6 @@
+SET LINESIZE 100
+SET PAGESIZE 50
+show parameter db_recovery_file_dest;
+SPOOL OFF
+EXIT;
+
---
+++ .idea/servercheck/sqlscripts/sql_parameters.sql
@@ -0,0 +1,15 @@
+SET LINESIZE 100
+SET PAGESIZE 50
+select name, value
+from v$parameter
+where name = 'open_cursors'
+   or name = 'processes'
+   or name = 'sessions'
+   or name = 'memory_target'
+   or name = 'sga_max_size'
+   or name = 'sga_target'
+   or name = 'pga_aggregate_target'
+   or name = 'undo_tablespace'
+   or name = 'undo_retention';
+SPOOL OFF
+EXIT;
